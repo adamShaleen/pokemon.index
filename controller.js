2e70c6ff0ebd67df2cb8e8251fd7f47f02aa0d0d
adamShaleen <adamshaleen@gmail.com>
2016-05-20 10:43:38 -0600

    refactored everything.

--- controller.js
+++ controller.js
@@ -1,8 +1,14 @@
 angular.module('Pokemon').controller("Controller", function($scope, Service) {
 
+//toggle show/hide
+$scope.response = false;
+$scope.toggle = function() {
+    $scope.response = !$scope.response;
+};
+
+//display Pokemon info
 $scope.displayPokemon = function(num) {
     Service.getPokemon($scope.num).then(function(response) {
-        // console.log(response);
         $scope.response = response;
     });
 };
--- service.js
+++ service.js
@@ -1,11 +1,11 @@
 angular.module('Pokemon').service('Service', function($http) {
 
+//Pokemon API
 this.getPokemon = function(num) {
   return $http ({
       method: 'GET',
       url: 'http://pokeapi.co/api/v2/pokemon/' + num + '/'
   }).then(function(response) {
-      console.log(response);
       return response.data;
   });
 };
